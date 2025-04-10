7af0d2aefb320245f59440877659f52464c7c8a7
Sattwik Sahu <sattwik21@iiserb.ac.in>
2025-04-10 17:44:11 +0530

    chores:refactored time convenience function name

--- src/yatry/utils/helpers/time.py
+++ src/yatry/utils/helpers/time.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import stats
 
 
-def create_time_convenience_func(
+def calc_time_conv_params(
     t_min: float, t_max: float, m_range: float = 0.8
 ) -> tuple[float, float]:
     """
@@ -38,6 +38,6 @@ def bhattacharyya_coeff(u1: float, u2: float, std1: float, std2: float) -> float
 def time_affinity_score(
     t1_min: float, t2_min: float, t1_max: float, t2_max: float, m_range: float = 0.8
 ) -> float:
-    u1, std1 = create_time_convenience_func(t_min=t1_min, t_max=t1_max, m_range=m_range)
-    u2, std2 = create_time_convenience_func(t_min=t2_min, t_max=t2_max, m_range=m_range)
+    u1, std1 = calc_time_conv_params(t_min=t1_min, t_max=t1_max, m_range=m_range)
+    u2, std2 = calc_time_conv_params(t_min=t2_min, t_max=t2_max, m_range=m_range)
     return bhattacharyya_coeff(u1=u1, u2=u2, std1=std1, std2=std2)
