ac73b83c156230920d1b941a74eaea06d4c89b0b
Sattwik Kumar Sahu <sattwik21@iiserb.ac.in>
2025-04-23 16:06:46 +0530

    chores: integration started

--- src/yatry/utils/models/__init__.py
+++ src/yatry/utils/models/__init__.py
@@ -12,4 +12,4 @@ class Passenger:
 
     def get_dep_time_range_num(self) -> tuple[float, float]:
         t_start, t_end = self.dep_time_range
-        return t_start.timestamp(), t_end.timestamp()
+        return t_start.timestamp() / 1000, t_end.timestamp() / 1000
